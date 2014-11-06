2655260a4f5e4a236d6df20458b445038be54816
pointsense <rizkysatriani@yahoo.com>
2014-11-07 01:54:21 +0700

    my function wp
    function code collection

---
+++ function.php
@@ -0,0 +1,39 @@
+<?php
+
+//set limit wp excerpt with total word, excecute by echo excerpt(50); this means, it will show 50 word
+function content($limit) {
+ $content = explode(' ', get_the_content(), $limit);
+ if (count($content)>=$limit) {
+ array_pop($content);
+ $content = implode(" ",$content).'...';
+ } else {
+ $content = implode(" ",$content);
+ }
+ $content = preg_replace('/[.+]/','', $content);
+ $content = apply_filters('the_content', $content);
+ $content = str_replace(']]>', ']]&gt;', $content);
+ return $content;
+}
+
+// this is for option theme plugin
+if ( !function_exists( 'of_get_option' ) ) {
+function of_get_option($name, $default = false) {
+	
+	$optionsframework_settings = get_option('optionsframework');
+	
+	// Gets the unique option id
+	$option_name = $optionsframework_settings['id'];
+	
+	if ( get_option($option_name) ) {
+		$options = get_option($option_name);
+	}
+		
+	if ( isset($options[$name]) ) {
+		return $options[$name];
+	} else {
+		return $default;
+	}
+}
+}
+
+?>
